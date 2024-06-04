607ad8e10351facd885ec1c098be735c878d0119
Kasun Fernando <fernandokasun672@gmail.com>
2024-06-04 23:35:56 +0530

    created my course component & new course component

--- src/app/components/main/dashboard/inner/teacher/my-courses/my-courses.component.ts
+++ src/app/components/main/dashboard/inner/teacher/my-courses/my-courses.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatOption} from "@angular/material/autocomplete";
 import {MatSelect} from "@angular/material/select";
@@ -9,6 +9,9 @@ import {NgForOf, NgIf} from "@angular/common";
 import {RouterLink} from "@angular/router";
 import {MatIcon} from "@angular/material/icon";
 import {MatTooltip} from "@angular/material/tooltip";
+import {Title} from "@angular/platform-browser";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-my-courses',
@@ -32,6 +35,32 @@ import {MatTooltip} from "@angular/material/tooltip";
   templateUrl: './my-courses.component.html',
   styleUrl: './my-courses.component.scss'
 })
-export class MyCoursesComponent {
+export class MyCoursesComponent implements OnInit{
+
+  loading = false;
+
+  constructor(private title:Title,
+              private db:AngularFirestore,
+              private cookieService:CookieService) {
+  }
+
+  ngOnInit(): void {
+
+    this.loading = true;
+
+    this.title.setTitle('My Courses | Pilot LMS');
+
+    this.db.collection('courses',ref=>ref.where(
+      'teacherId','==', atob(this.cookieService.get('teacherId')))).get().subscribe(querySnapshot=>{
+        const documents = querySnapshot.docs.map(doc => doc.data());
+        if (documents){
+          console.log(documents);
+          console.log('have');
+        }else{
+          console.log('haven');
+        }
+    })
+
+  }
 
 }
--- src/app/components/main/dashboard/inner/teacher/new-course/new-course.component.ts
+++ src/app/components/main/dashboard/inner/teacher/new-course/new-course.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
+import {MatIcon} from "@angular/material/icon";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
+import {MatProgressBar} from "@angular/material/progress-bar";
 
 @Component({
   selector: 'app-new-course',
   standalone: true,
-    imports: [
-        MatFormField,
-        MatInput,
-        MatLabel
-    ],
+  imports: [
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatIcon,
+    MatIconButton,
+    RouterLink,
+    MatButton,
+    MatProgressBar
+  ],
   templateUrl: './new-course.component.html',
   styleUrl: './new-course.component.scss'
 })
