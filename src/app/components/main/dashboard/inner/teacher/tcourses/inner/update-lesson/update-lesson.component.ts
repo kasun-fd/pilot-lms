e3188323e9c23de4ff333596bb93737d91e64aae
Kasun Fernando <fernandokasun672@gmail.com>
2024-06-05 22:53:39 +0530

    update-lesson.component.ts updated

--- src/app/components/main/dashboard/inner/teacher/tcourses/inner/update-lesson/update-lesson.component.ts
+++ src/app/components/main/dashboard/inner/teacher/tcourses/inner/update-lesson/update-lesson.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatIcon} from "@angular/material/icon";
@@ -7,6 +7,8 @@ import {MatInput} from "@angular/material/input";
 import {MatProgressBar} from "@angular/material/progress-bar";
 import {NgIf} from "@angular/common";
 import {ActivatedRoute, RouterLink} from "@angular/router";
+import {Title} from "@angular/platform-browser";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
 
 @Component({
   selector: 'app-update-lesson',
@@ -30,11 +32,39 @@ import {ActivatedRoute, RouterLink} from "@angular/router";
 export class UpdateLessonComponent {
   loading = false;
   selectedId:any;
+  docsCount:any;
 
-  constructor(private activeRouter:ActivatedRoute) {
+  form = new FormGroup({
+    index:new FormControl('',[
+      Validators.required
+    ]),
+    title:new FormControl('',[
+      Validators.required
+    ]),
+    des:new FormControl('',[
+      Validators.required
+    ])
+  })
+
+  constructor(private activeRouter:ActivatedRoute,
+              private title:Title,
+              private db:AngularFirestore) {
+
+    this.title.setTitle('Update Lesson | Pilot LMS');
+
     activeRouter.paramMap.subscribe(resp=>{
       this.selectedId = resp.get('id');
     })
+
+    this.db.collection('lessons').get().subscribe(querySnapshot=>{
+      querySnapshot.docs.map(doc=>{
+        // @ts-ignore
+        console.log(doc.data().length)
+      })
+    })
+
   }
+
+  allLesson(){}
 
 }
